6223770be4284c8af65c75b3ab7af2b3c5d917b7
GreenAcidy <47273458+GreenAcidy@users.noreply.github.com>
2022-02-27 22:48:30 +0300

    Complete task 6

--- NorthwindServiceCoreClient/NorthwindServiceCoreClient/Program.cs
+++ NorthwindServiceCoreClient/NorthwindServiceCoreClient/Program.cs
@@ -11,6 +11,8 @@ namespace NorthwindServiceCoreClient // Note: actual namespace depends on the pr
             const string serviceUri = "https://services.odata.org/V3/Northwind/Northwind.svc/";
             var entities = new NorthwindModel.NorthwindEntities(new Uri(serviceUri)); // breakpoint #1.1
 
+            ManualResetEventSlim mre = new ManualResetEventSlim(); // (1) - Инициализация примитива синхронизации "событие".
+
             IAsyncResult asyncResult = entities.Employees.BeginExecute((ar) =>
             {
                 var employees = entities.Employees.EndExecute(ar); // breakpoint #1.2
@@ -20,9 +22,12 @@ namespace NorthwindServiceCoreClient // Note: actual namespace depends on the pr
                 {
                     Console.WriteLine("\t{0} {1}", person.FirstName, person.LastName);
                 }
+
+                mre.Set(); // (2) - Отправить сигнал методу WaitAll.
+
             }, null);
 
-            WaitHandle.WaitAny(new[] { asyncResult.AsyncWaitHandle }); // breakpoint #1.3
+            WaitHandle.WaitAny(new[] { mre.WaitHandle }); // (3) - Блокировать поток выполнения, пока не будет получен сигнал.
         }
     }
 }
--- TripPinUnchaseCoreClient/TripPinUnchaseCoreClient/Program.cs
+++ TripPinUnchaseCoreClient/TripPinUnchaseCoreClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿const string serviceUri = "https://services.odata.org/TripPinRESTierService";
 var container = new Trippin.Container(new Uri(serviceUri));
 
+ManualResetEventSlim mre = new ManualResetEventSlim(); // (1) - Инициализация примитива синхронизации "событие".
+
 IAsyncResult asyncResult = container.People.BeginExecute((ar) =>
 {
     Console.WriteLine("People in TripPin service:");
@@ -11,9 +13,8 @@ IAsyncResult asyncResult = container.People.BeginExecute((ar) =>
         Console.WriteLine("\t{0} {1}", person.FirstName, person.LastName);
     }
 
+    mre.Set(); // (2) - Отправить сигнал методу WaitAll.
+
 }, null);
 
-WaitHandle.WaitAny(new[] { asyncResult.AsyncWaitHandle });
-
-Console.WriteLine("Press any key to continue.");
-Console.ReadLine();
+WaitHandle.WaitAny(new[] { mre.WaitHandle }); // (3) - Блокировать поток выполнения, пока не будет получен сигнал.
