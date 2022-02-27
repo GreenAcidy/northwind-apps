f86ea64cf1c2e4e0cd7a1c1e684e3bad930cceb0
GreenAcidy <47273458+GreenAcidy@users.noreply.github.com>
2022-02-27 22:39:56 +0300

    use breakpoints in async app

--- NorthwindServiceCoreAsyncClient/NorthwindServiceCoreAsyncClient/Program.cs
+++ NorthwindServiceCoreAsyncClient/NorthwindServiceCoreAsyncClient/Program.cs
@@ -9,20 +9,20 @@ namespace NorthwindServiceCoreAsyncClient // Note: actual namespace depends on t
         public static async Task Main(string[] args)
         {
             const string serviceUri = "https://services.odata.org/V3/Northwind/Northwind.svc/";
-            var entities = new NorthwindModel.NorthwindEntities(new Uri(serviceUri));
+            var entities = new NorthwindModel.NorthwindEntities(new Uri(serviceUri)); // breakpoint #2.1
 
             var employees = await Task<IEnumerable<NorthwindModel.Employee>>.Factory.FromAsync(
                 entities.Employees.BeginExecute(null, null),
-                (iar) => entities.Employees.EndExecute(iar));
+                (iar) =>
+                {
+                    return entities.Employees.EndExecute(iar); // breakpoint #2.2
+                });
 
-            Console.WriteLine("Employees in Northwind service:");
+            Console.WriteLine("Employees in Northwind service:"); // breakpoint #2.3
             foreach (var person in employees)
             {
                 Console.WriteLine("\t{0} {1}", person.FirstName, person.LastName);
             }
-
-            Console.WriteLine("Pressny key to continue.");
-            Console.ReadLine();
         }
     }
 }
