8e47a4f7d8cad4df095b7293f164afb4ff0a7440
GreenAcidy <47273458+GreenAcidy@users.noreply.github.com>
2022-02-27 18:25:15 +0300

    Complete task 2

---
+++ TripPinUnchaseCoreAsyncClient/TripPinUnchaseCoreAsyncClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TripPinUnchaseCoreAsyncClient 
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            const string serviceUri = "https://services.odata.org/TripPinRESTierService";
+            var container = new Trippin.Container(new Uri(serviceUri));
+
+            Console.WriteLine("People in TripPin service:");
+            var people = await container.People.ExecuteAsync();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine("\t{0} {1}", person.FirstName, person.LastName);
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
